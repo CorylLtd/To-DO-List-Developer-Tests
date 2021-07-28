ebab05d08c545428977e3962d5f9eef4ef86b777
Martin Randall <martin@coryl.com>
2021-07-28 10:26:27 +0100

    Added todo component

---
+++ blazor/todo-list-blazor/code/TodoItem.cs
@@ -0,0 +1,11 @@
+namespace todo_list_blazor.Code
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
